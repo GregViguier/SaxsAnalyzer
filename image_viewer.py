21d36c03bd35db6595d90e3c42cd47aff3e678ec
GregViguier <gregory_v@icloud.com>
2015-09-22 22:21:50 +0200

    First import.

--- image_viewer.py
+++ image_viewer.py
@@ -3,16 +3,18 @@ import matplotlib.pyplot as plt
 import hdfReader
 
 
-def plotImage(arr):
+def plot_image(arr):
     fig = plt.figure(figsize=(5, 5), dpi=80, facecolor='w', edgecolor='w', frameon=True)
     imAx = plt.imshow(arr, origin='lower', interpolation='nearest')
     fig.colorbar(imAx, pad=0.01, fraction=0.1, shrink=1.00, aspect=20)
 
+
 if __name__ == '__main__':
-    imageData = hdfReader.load_image_at_index(
-        '/home/gregory/Work/Samples/NeXuS/ImageReducer/200Images/elisabeth_0043_2013-10-05_03-36-44.nxs',
-        'R16-19iso_0043', 5)
+    file_name = r'/home/gregory/Work/Samples/NeXuS/ImageReducer/200Images/elisabeth_0043_2013-10-05_03-36-44.nxs'
+    file_name = r'/home/gregory/Dropbox/elisabeth_0044_2013-10-05_04-10-19.nxs'
+
+    imageData = hdfReader.load_image_at_index(file_name, 'buffer_0044', 5)
     # Put it in the viewer
-    plotImage(imageData)
+    plot_image(imageData)
     # Show it
     plt.show()
