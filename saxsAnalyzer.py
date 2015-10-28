48228672bff1b9e9816b0936db8b9e4e050eca3c
GregViguier <gregory_v@icloud.com>
2015-10-28 18:01:20 +0100

    Add Timer for benchmarking

--- saxsAnalyzer.py
+++ saxsAnalyzer.py
@@ -2,18 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyFAI.azimuthalIntegrator
 import hdfReader
-import pandas as pd
+import time
 
-ai = pyFAI.AzimuthalIntegrator()
-
-# DATA
-data = hdfReader.load_image_at_index(
-    r'/home/gregory/Dropbox/elisabeth_0044_2013-10-05_04-10-19.nxs',
-    'buffer_0044', 5)
-
+start_time = time.time()
 # Load MASK
 mask = np.genfromtxt("GV2.txt", delimiter=";", dtype=int)
-
+ai = pyFAI.AzimuthalIntegrator()
 
 ai.set_wavelength(0.0000000001033)
 ai.set_dist(1.499)
@@ -23,21 +17,17 @@ ai.set_mask(mask)
 ai.set_poni2(0)
 ai.set_poni1(0)
 
-print ai
-q, I, Sigma = ai.integrate1d(
-    data, 1024, unit="q_nm^-1", error_model="poisson",
-    polarization_factor=1.0, method="numpy")
 
-print("Plotting !!")
-Iserie = pd.Series(I)
-Qserie = pd.Series(q)
-Sserie = pd.Series(Sigma)
-dataSerie = pd.DataFrame(Iserie)
-dataSerie['Q'] = Qserie
-# dataSerie['Sigma'] = Sserie
-dataSerie.plot(x='Q')
+for x in range(0, 49):
+    # DATA
+    data = hdfReader.load_image_at_index(
+        r'/home/gregory/Dropbox/elisabeth_0044_2013-10-05_04-10-19.nxs',
+        'buffer_0044', x)
+
+    q, I, Sigma = ai.integrate1d(
+        data, 1024, unit="q_nm^-1", error_model="poisson",
+        polarization_factor=None, method="cython")
+    plt.errorbar(q, I, Sigma)
+
+print("--- %s seconds ---" % (time.time() - start_time))
 plt.show()
-
-
-#plt.errorbar(q, I, Sigma)
-#plt.show()
