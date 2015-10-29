25ceba7a5bdf57e460b1000726d108bbd7a6a16c
GregViguier <gregory_v@icloud.com>
2015-10-29 17:37:10 +0100

    code formatting

--- saxsAnalyzer.py
+++ saxsAnalyzer.py
@@ -14,9 +14,11 @@ entryname = 'buffer_0044'
 
 def worker(index):
     data = hdfReader.load_image_at_index(filename, entryname, index)
-    q, I, Sigma = ai.integrate1d(
-        data, 1024, mask=mask, unit="q_A^-1", error_model="poisson",
-        polarization_factor=None, method="cython")
+    q, I, Sigma = ai.integrate1d(data, 1024,
+                                 mask=mask, unit="q_A^-1",
+                                 error_model="poisson",
+                                 polarization_factor=None,
+                                 method="cython")
     return index, q, I, Sigma
 
 start_time = time.time()
@@ -42,10 +44,7 @@ for t in mapResult:
 
 fontP = FontProperties()
 fontP.set_size('small')
-#plt.legend(range(0,49),bbox_to_anchor=(0.5, -0.5), loc='lower center', fancybox=True, shadow=True, ncol=2)
-
-
-
+# plt.legend(range(0,49),bbox_to_anchor=(0.5, -0.5), loc='lower center', fancybox=True, shadow=True, ncol=2)
 
 print("--- %s seconds ---" % (time.time() - start_time))
 
