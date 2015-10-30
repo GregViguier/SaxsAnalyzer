6fec6fd90d3e6058f25c448f522fef846f3e4256
GregViguier <gregory_v@icloud.com>
2015-10-30 16:10:11 +0100

    Code cleanup Remove unused files Use bokeh for rendering

--- saxsAnalyzer.py
+++ saxsAnalyzer.py
@@ -5,8 +5,9 @@ import hdfReader
 import time
 import concurrent.futures
 from matplotlib.font_manager import FontProperties
-
-plt.style.use('ggplot')
+from bokeh.models import HoverTool, BoxSelectTool
+from bokeh.plotting import figure, show, output_file, vplot, output_server
+from bokeh.palettes import Spectral11
 
 filename = r'/home/gregory/Dropbox/elisabeth_0044_2013-10-05_04-10-19.nxs'
 entryname = 'buffer_0044'
@@ -19,7 +20,7 @@ def worker(index):
                                  error_model="poisson",
                                  polarization_factor=None,
                                  method="cython")
-    return index, q, I, Sigma
+    return q, I
 
 start_time = time.time()
 
@@ -38,14 +39,18 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
     mapResult = executor.map(worker, range(49))
 
 
-fig, ax = plt.subplots()
-for t in mapResult:
-    ax.plot(t[1], t[2])
+output_file("legend.html", title="legend.py example")
+#output_server("line")
+tools = "pan,wheel_zoom,box_zoom,reset,resize"
 
-fontP = FontProperties()
-fontP.set_size('small')
-# plt.legend(range(0,49),bbox_to_anchor=(0.5, -0.5), loc='lower center', fancybox=True, shadow=True, ncol=2)
+p = figure(title="CircleGathering", tools=tools)
+
+mypalette = Spectral11[0:49]
+
+for index, t in enumerate(mapResult):
+    p.line(t[0], t[1], legend="Radial Intensity " +
+           str(index), line_color=mypalette[index % 11])
 
 print("--- %s seconds ---" % (time.time() - start_time))
 
-plt.show()
+show(p)
