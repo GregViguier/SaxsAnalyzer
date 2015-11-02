4078c82795aa17984be63ef53ae2e988ed281ba5
GregViguier <gregory_v@icloud.com>
2015-11-02 08:16:52 +0100

    Use python-nvd3

--- app.py
+++ app.py
@@ -2,13 +2,35 @@ from flask import Flask
 from flask import render_template
 import saxsAnalyzer
 import json
+from nvd3 import lineWithFocusChart
+from nvd3 import lineChart
 app = Flask(__name__)
 
 
+@app.route("/test")
+def indextest():
+    chart = lineWithFocusChart(show_labels=False, name='lineWithFocusChart', x_is_date=True, x_axis_format="%d %b %Y")
+    chart.show_legend(False)
+    xdata = [1365026400000000, 1365026500000000, 1365026600000000,
+             1365026700000000, 1365026800000000, 1365026900000000, 1365027000000000]
+    ydata = [-6, 5, -1, 2, 4, 8, 10]
+
+    extra_serie = {"tooltip": {"y_start": "", "y_end": " ext"},
+                       "date_format": "%d %b %Y"}
+    chart.add_serie(name="Serie 1", y=ydata, x=xdata, extra=extra_serie)
+    chart.buildhtml()
+    return chart.htmlcontent
+
 @app.route("/")
 def index():
-    return render_template("index.html")
-
+    chart = lineWithFocusChart(name='lineWithFocusChart')
+    #chart = lineChart(name='lineChart')
+    data = saxsAnalyzer.getdata()
+    for index,saxsdata in enumerate(data):
+    	if index < 5 :
+    		chart.add_serie(name="Serie " + str(index), y=saxsdata[1], x=saxsdata[0])
+    chart.buildhtml()
+    return chart.htmlcontent
 
 @app.route("/saxs")
 def circlegathering():
