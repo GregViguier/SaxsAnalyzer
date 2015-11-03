fc620c36f1ec211695b3b46898a33f8bf16f08ef
GregViguier <gregory_v@icloud.com>
2015-11-03 11:02:12 +0100

    JSON serialization corrections

--- app.py
+++ app.py
@@ -1,45 +1,35 @@
 from flask import Flask
-from flask import render_template
 import saxsAnalyzer
 import json
 from nvd3 import lineWithFocusChart
 from nvd3 import lineChart
+from io import StringIO
 app = Flask(__name__)
 
 
-@app.route("/test")
-def indextest():
-    chart = lineWithFocusChart(show_labels=False, name='lineWithFocusChart', x_is_date=True, x_axis_format="%d %b %Y")
-    xdata = [1365026400000000, 1365026500000000, 1365026600000000,
-             1365026700000000, 1365026800000000, 1365026900000000, 1365027000000000]
-    ydata = [-6, 5, -1, 2, 4, 8, 10]
-
-    extra_serie = {"tooltip": {"y_start": "", "y_end": " ext"},
-                       "date_format": "%d %b %Y"}
-    chart.add_serie(name="Serie 1", y=ydata, x=xdata, extra=extra_serie)
-    chart.buildhtml()
-    return chart.htmlcontent
-
 @app.route("/")
 def index():
-    chart = lineWithFocusChart(show_legend=False, name='lineWithFocusChart')
-    #chart = lineChart(show_legend=False, name='lineChart')
-    extra_serie = {"tooltip": {"y_start": "", "y_end": " ext"}, "date_format": "%d %b %Y"}
-    data = saxsAnalyzer.getdata()
-    for index,saxsdata in enumerate(data):
-    	if index < 5 :
-    		chart.add_serie(name="Serie " + str(index), y=saxsdata[1], x=range(len(saxsdata[1])), extra=extra_serie)
+    chart = lineWithFocusChart(
+        height=600, width='800', show_legend=False, name='lineWithFocusChart')
+    data = saxsAnalyzer.getdata(5)
+    for index, saxsdata in enumerate(data):
+        chart.add_serie(name="Serie " + str(index),
+                        y=saxsdata[1], x=range(len(saxsdata[1])))
     chart.buildhtml()
     return chart.htmlcontent
+
 
 @app.route("/saxs")
 def circlegathering():
-    data = saxsAnalyzer.getdata()
-    alldata = []
-    for saxsdata in data:
-        alldata.append(saxsdata[1].tolist())
-    json_data = json.dumps(alldata)
-    return json_data
+    data = saxsAnalyzer.getdata(5)
+    json = "["
+    for index,saxsdata in enumerate(data):
+        I = saxsdata[1]
+        Q = saxsdata[0]
+        for ivalue,qvalue in zip(I,Q):
+            json += "{\"name\": \"Azimuthal " + str(index) + "\"" + ", \"Q\" :" +str(qvalue) + ", \"I\" :" + str(ivalue) + "},"
+        json += "]"
+    return json
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
--- saxsAnalyzer.py
+++ saxsAnalyzer.py
@@ -5,7 +5,7 @@ import concurrent.futures
 from itertools import repeat
 
 
-def getdata():
+def getdata(nb_images=49):
     # Load MASK
     mask = np.genfromtxt("GV2.txt", delimiter=";",
                          dtype=int)
@@ -18,13 +18,12 @@ def getdata():
     ai.set_poni2(0)
 
     executor = concurrent.futures.ThreadPoolExecutor(max_workers=5)
-    mapresult = executor.map(worker, *(range(49), repeat(ai), repeat(mask)))
+    mapresult = executor.map(worker, *(range(nb_images), repeat(ai), repeat(mask)))
     return mapresult
 
 
 def worker(index, ai, mask):
     filename = r'/home/gregory/Dropbox/elisabeth_0044_2013-10-05_04-10-19.nxs'
-    print(index)
     data = hdfReader.load_image_at_index(filename, 'buffer_0044', index)
     q, I, Sigma = ai.integrate1d(data, 1024,
                                  mask=mask, unit="q_A^-1",
